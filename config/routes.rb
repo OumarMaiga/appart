5242015ee774b54e32fe26ab0115d0a65e943680
Hasseye <oumarm611@gmail.com>
2019-09-25 22:11:35 +0000

    Mise en place du squelette de recherche sans appart

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'pages/users'
   root to: "pages#index"
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  get 'pages/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
