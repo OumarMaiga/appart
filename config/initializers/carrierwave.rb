0ad7a7277c1e8eb46548492b407a39fc66a9ed95
Hasseye <oumarm611@gmail.com>
2020-05-13 16:14:11 +0000

    Hide keys aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
         provider: "AWS",
         region: "us-east-2",
-        aws_access_key_id: "AKIAIGACB2CKDMIHLT4A",
-        aws_secret_access_key: "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"
+        aws_access_key_id: "*************************************",
+        aws_secret_access_key: "***************************************"
     }
     config.fog_directory = "appartml"
 end
