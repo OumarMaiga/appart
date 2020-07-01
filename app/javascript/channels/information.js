98d5792ac5c81b636d8c547dff2c133d1f857c4d
Hasseye <oumarm611@gmail.com>
2020-07-01 00:38:06 +0000

    Design responsive And info alert or notice

--- app/javascript/channels/information.js
+++ app/javascript/channels/information.js
@@ -1,7 +1,7 @@
 const info = function() {
     $(document).on('turbolinks:load', function() {
         console.log('Info');
-        $('.info').fadeOut(5000);
+        $('.info').fadeOut(8000);
     })
 }
 
