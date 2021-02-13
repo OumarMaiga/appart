58123c45406e07ce32dbe4706b16945267176a9b
hasseye <oumarm611@gmail.com>
2021-02-13 20:19:39 +0000

    AW3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
         provider: "AWS",
         region: "us-east-2",
-        aws_access_key_id: "AKIAJB6M6F5YQ6UVKRAQ"
+        aws_access_key_id: "AKIAJB6M6F5YQ6UVKRAQ",
         aws_secret_access_key: "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"
     }
     config.fog_directory = "appartml"
